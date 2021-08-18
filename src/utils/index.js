bb9d57d31d2c5e954e8b197412a1515fdd9f2015
Jack_Chen <cjl1179822675@163.com>
2021-08-18 10:33:16 +0800

    添加了useMount和useDebounce两个Hook

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => (value === 0 ? true : !!value);
 
 export const cleanObject = (object) => {
@@ -9,4 +11,28 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+    // eslint-disable-next-line
+  }, []);
+};
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedValue(value);
+      console.log("定时器已建立");
+    }, delay);
+    return () => {
+      clearTimeout(timeout);
+      console.log("定时器已销毁");
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
 };

07a77d27f66e4025ca0bf18857c178a3aa562195
Jack_Chen <cjl1179822675@163.com>
2021-08-24 09:37:41 +0800

    使用useRef解决潜在的闭包问题

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const isFalsy = (value: unknown) => (value === 0 ? true : !!value);
 
@@ -43,7 +43,7 @@ export const useDebounce = <V>(value: V, delay?: number) => {
 };
 
 export const useDocumentTitle = (title: string, keepOnUnmount = true) => {
-  const oldTitle = document.title;
+  const oldTitle = useRef(document.title).current;
   useEffect(() => {
     document.title = title;
   }, [title]);
@@ -53,6 +53,5 @@ export const useDocumentTitle = (title: string, keepOnUnmount = true) => {
         document.title = oldTitle;
       }
     };
-    //eslint-disable-next-line
-  }, []);
+  }, [keepOnUnmount, oldTitle]);
 };
